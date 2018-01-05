419c375c7fd22b6194a53067fde28dff48480589
Ville Peurala <ville.peurala@gmail.com>
2018-01-05 20:43:11 +0200

    First version.

---
+++ create_uninstaller_script.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+if [[ "$#" -eq 0 ]]; then
+	printf "Missing mandatory parameter: DEVELOPER_NAME.\n";
+	printf "Usage: $0 DEVELOPER_NAME\n";
+	printf "Example: $0 \"Native Instruments\"\n";
+	exit 1;
+fi
+
+DEVELOPER_NAME=$1;
+DEVELOPER_NAME_IN_LOWER_SNAKE_CASE=$(echo $DEVELOPER_NAME | tr '[:upper:]' '[:lower:]' | sed 's/ /_/g');
+
+UNINSTALLER_SCRIPT_FILE_NAME="uninstall_${DEVELOPER_NAME_IN_LOWER_SNAKE_CASE}_plugins.txt";
+PLUGINS_LIST_FILE_NAME="${DEVELOPER_NAME_IN_LOWER_SNAKE_CASE}_plugins.txt";
+
+if [[ ! -f "template.sh" ]]; then
+	printf "\e[91mFile \e[32;1mtemplate.sh \e[91;21m is not found in this directory. It is required for this script to work.\e[0m\n";
+	exit 66;
+fi
+
+cat template.sh | sed "s/__DEVELOPER__/$1/" > ${UNINSTALLER_SCRIPT_FILE_NAME};
+chmod 755 ${UNINSTALLER_SCRIPT_FILE_NAME};
+
+printf "Uninstaller script \e[92m${UNINSTALLER_SCRIPT_FILE_NAME}\e[0m created.\n";
+printf "Now you must create a file listing the plugins from \e[94m${DEVELOPER_NAME}\e[0m which you want to remove.\n";
+printf "The name of the plugin list file must be \e[32;1m${PLUGINS_LIST_FILE_NAME}\e[0m, and it must contain one plugin name per line.\n";
+printf "See the files in directory \e[32;1mexamples\e[0m for an example.\n";
+printf "After you have created the plugin list file (\e[32;1m${PLUGINS_LIST_FILE_NAME}\e[0m), you can run the uninstaller script file (\e[92m${UNINSTALLER_SCRIPT_FILE_NAME}\e[0m).\n"
+printf "It must be run as root, so use sudo. For example: \e97;1msudo ${UNINSTALLER_SCRIPT_FILE_NAME}\e[0m\n";
+printf "It will only work correctly if you have installed the plugins in their default directories.\n";
+printf "If you have installed them somewhere else, you must manually remove some files after running \e[92m${UNINSTALLER_SCRIPT_FILE_NAME}\e[0m; still, it is useful to run the uninstaller script first, because then you will have less manual work afterwards than if you removed the plugins completely manually.\n";
+
---
+++ template.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+DEVELOPER="__DEVELOPER__";
+PLUGINS_LIST_FILE_NAME=$(echo "$DEVELOPER" | tr '[:upper:]' '[:lower:]' | sed 's/ /_/g' | sed 's/$/_plugins.txt/g');
+PLIST_PREFIX=$(echo "$DEVELOPER" | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g' | sed 's/^/com./g');
+printf "\e[91mThis script is going to remove all your plugins from \e[96m${DEVELOPER} \e[95m(${PLIST_PREFIX}.*).\e[0m\n";
+
+if [[ ! -f ${PLUGINS_LIST_FILE_NAME} ]]; then
+	printf "\e[91mFile \e[32;1m${PLUGINS_LIST_FILE_NAME} \e[91;21m is not found in this directory. It is required for listing the plugins to be removed.\e[0m\n";
+	exit 66;
+fi
+
+printf "\e[91mThis means the following plugins (read from file \e[32m${PLUGINS_LIST_FILE_NAME}):\e[0m\n";
+printf "\e[34;4m%s\e[0m\n" "$(cat ${PLUGINS_LIST_FILE_NAME} | awk 'NF {print $0}' | cat -b)";
+
+if [[ "$EUID" -ne "0" ]]; then
+	printf "\e[91mThis script can only be run as root. Please run with sudo:\e[39;1m sudo ./$(basename $0)\n";
+	exit 77;
+fi
+
+printf "Are you sure? [y/n] ";
+until [[ -n "$confirmation" ]]; do
+	read -n 1 -r confirmation
+	case "$confirmation" in
+		[yY])
+			true;
+			;;
+		[nN])
+			false;
+			printf "\nOperation aborted.\n";
+			exit 0;
+			;;
+		*)
+			printf "\nPlease answer y or n. ";
+			confirmation=;
+			;;
+	esac;
+done
+printf "\nConfirmation obtained. Proceeding to delete the plugins.\n";
+
+IFS=$'\n';
+for PLUGIN in $(awk 'NF {print "\""$0"\""}' ${PLUGINS_LIST_FILE_NAME}); do
+	printf "\e[95m--- ${PLUGIN} ---\e[0m\n";
+	set -x;
+	rm -rf "/Applications/${DEVELOPER}/${PLUGIN}";
+	rm -rf "/Library/Audio/Plug-Ins/VST/${PLUGIN}.vst";
+	rm -rf "/Library/Audio/Plug-Ins/Components/${PLUGIN}.component";
+	rm -rf "/Library/Application Support/Avid/Audio/Plug-Ins/${PLUGIN}.aaxplugin";
+	rm -rf "${HOME}/Documents/${DEVELOPER}/${PLUGIN}";
+	rm -rf "${HOME}/Library/Preferences/com.native-instruments.${PLUGIN}.plist";
+	rm -rf "/Library/Application Support/${DEVELOPER}/${PLUGIN}";
+	# NKS Settings: These are always under "Native Instruments" and "com.native-instruments", regardless of the developer.
+	rm -rf "/Library/Application Support/Native Instruments/Service Center/${PLUGIN}.xml";
+	rm -rf "/Library/Preferences/com.native-instruments.${PLUGIN}.plist";
+done;
+
