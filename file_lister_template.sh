de57b8e2300ae4b556a6c70f388bfb3fc8f0ff23
Ville Peurala <ville.peurala@gmail.com>
2018-01-06 17:03:58 +0200

    Little fixes for file lister.

--- file_lister_template.sh
+++ file_lister_template.sh
@@ -12,9 +12,15 @@ fi
 printf "\e[91mThis means the following plugins (read from file \e[32m${PLUGINS_LIST_FILE_NAME}):\e[0m\n";
 printf "\e[34;4m%s\e[0m\n" "$(cat ${PLUGINS_LIST_FILE_NAME} | awk 'NF {print $0}' | cat -b)";
 
+if [[ "$EUID" -ne "0" ]]; then
+	printf "\e[91mThis script can only be run as root. Please run with sudo:\e[39;1m sudo ./$(basename $0)\n";
+	exit 77;
+fi
+
 IFS=$'\n';
 for PLUGIN in $(awk 'NF {print $0}' ${PLUGINS_LIST_FILE_NAME}); do
 	printf "\e[95m--- ${PLUGIN} ---\e[0m\n";
+    set -x;
 	ls "/Applications/${DEVELOPER}/${PLUGIN}";
 	ls "/Library/Audio/Plug-Ins/VST/${PLUGIN}.vst";
 	ls "/Library/Audio/Plug-Ins/Components/${PLUGIN}.component";
@@ -25,5 +31,6 @@ for PLUGIN in $(awk 'NF {print $0}' ${PLUGINS_LIST_FILE_NAME}); do
 	# NKS Settings: These are always under "Native Instruments" and "com.native-instruments", regardless of the developer.
 	ls "/Library/Application Support/Native Instruments/Service Center/${PLUGIN}.xml";
 	ls "/Library/Preferences/com.native-instruments.${PLUGIN}.plist";
+    { set +x; } 2>/dev/null;
 done;
 
